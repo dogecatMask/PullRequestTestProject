2405bef8a1e09c711e32b231576af00018da9add
Yekang Wu <yekang.wu@wsu.edu>
2022-05-10 15:07:16 -0700

    fifth commit - develop

--- Test004.java
+++ Test004.java
@@ -3,9 +3,8 @@
  * @version 1.0
  * @date 2022-05-10 14:24
  */
-public class Test001 {
+public class Test004 {
     public static void main(String[] args) {
-        System.out.println("Good weather 001!");
-        System.out.println("develop branch 001");
+        System.out.println("slow-fix branch 004");
     }
 }
