595babad314345656f1f987c24af39e596d4d5a4
Yekang Wu <yekang.wu@wsu.edu>
2022-06-07 09:56:36 -0700

    6th commit - develop

--- Test004.java
+++ Test004.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author David Wu
  * @version 1.0
@@ -6,5 +8,21 @@
 public class Test004 {
     public static void main(String[] args) {
         System.out.println("slow-fix branch 004");
+        StringBuilder sb;
+        sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append(String.valueOf(i));
+            sb.append("  ");
+        }
+        String resultStr = sb.toString().trim();
+        System.out.println("result: " + resultStr);
+        System.out.println("=======================");
+
+        String[] strarray = resultStr.split("  ");
+        System.out.println("resutl2: " + Arrays.toString(strarray));
+        System.out.println("=======================");
+
+        sb = new StringBuilder("I love");
+        System.out.println("result:"+sb.toString());
     }
 }
